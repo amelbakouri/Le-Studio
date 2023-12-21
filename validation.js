9fd377b1a672f057243002e755b692ec731db6d9
amelbakouri <amelbakouri4@gmail.com>
2023-12-21 15:54:59 +0100

    hihiihihihi j'ai mal

--- validation.js
+++ validation.js
@@ -1,9 +1,17 @@
 const form =  document.querySelector('form');
-const emailInput = document.querySelector('input[type=email]')
-const mobileInput = document.querySelector('input[type=tel]')
-const nomInput = document.querySelector('input[type=text]')
-const prenomInput = document.querySelector('input[type=text]')
-const sujetInput = document.querySelector('input[type=text]')
+const nomInput = document.querySelector('#InputNom');
+const prenomInput = document.querySelector('#InputPrenom');
+const mobileInput = document.querySelector('#InputTel');
+const emailInput = document.querySelector('input[type=email]');
+const sujetInput = document.querySelector('#InputSujet');
+const msgInput = document.querySelector('#InputMessage');
+
+let nomValid = false; 
+let prenomValid = false; 
+let mobileValid = false;
+let emailValid = false; 
+let sujetValid = false; 
+let msgValid = false; 
 
 // console.log(form)
 
@@ -26,29 +34,41 @@ emailInput.addEventListener('input', (e)=>{
 }) 
 
 
-function addClass(element,regex,value) {
+function addClass(element,regex,value,valid) {
     if (regex.test(value)) {
         element.classList.add('is-valid')
         element.classList.remove('is-invalid') 
+        valid = true
     } else {
         element.classList.remove('is-valid')
         element.classList.add('is-invalid')
+        valid = false
     }
 }
 
-nomInput.addEventListener('input', e => addClass(nomInput,UserRegex, e.target.value));
-prenomInput.addEventListener('input', e => addClass(prenomInput,UserRegex, e.target.value));
+nomInput.addEventListener('input', e => addClass(nomInput,UserRegex, e.target.value,nomValid));
+prenomInput.addEventListener('input', e => addClass(prenomInput,UserRegex, e.target.value,prenomValid));
+sujetInput.addEventListener('input', e=>addClass(sujetInput,SujetRegex, e.target.value,sujetValid));
+emailInput.addEventListener('input', e=>addClass(emailInput,EmailRegex, e.target.value,emailValid));
+msgInput.addEventListener('input', e=>addClass(msgInput,MessageRegex, e.target.value,msgValid));
 mobileInput.addEventListener('input',e=>{
     let tel = e.target.value;
     tel = tel.replace(/ /g, '');
     tel = tel.replace(/^0/, '+33');
-    addClass(mobileInput, PhoneNumberRegex,tel)
+    addClass(mobileInput, PhoneNumberRegex,tel,mobileValid)
 })
 
-sujetInput.addEventListener('input', e=>addClass(sujetInput,SujetRegex, e.target.value))
-sujetInput.addEventListener('input', e=>addClass(sujetInput,SujetRegex, e.target.value))
- 
-
 form.addEventListener('submit', e=>{
-    e.preventDefault()
-    })
+    e.preventDefault();
+    if (nomValid && prenomValid && telValid && emailValid && sujetValid && msgValid) {
+      Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "lasalledesport90@gmail.com",
+        Password : "76C8286E090E4EFEE9F45EDD7FE41B69819F",
+        To : 'salle2sport@outlook.fr',
+        From : "lasalledesport90@gmail.com",
+        Subject : sujetValue,
+        Body : `${prenomValue} ${nomValue} qui a les coordonées suivantes :<br/> numéro de tel: ${telValue}<br/> adresse mail: ${emailValue}<br/> vous a envoyer le message suivant : <br/> ${msgValue}`
+      }).then(message => message == 'OK' && location.reload())
+    }
+  })
